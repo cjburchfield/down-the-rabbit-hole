ab21339e6bf81055817a7f027613c0eb8ee79712
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-03-09 16:51:54 -0500

    Getting ready for live push

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,14 +1,14 @@
 class Example {
     constructor(ele) {
         this.ele = ele
-        this.ele.innerHTML = "<h1>Drink Me</h1>"
+        this.ele.innerHTML = "<h1>🫖 Drink Me 🫖</h1>"
 
         this.handleClick = this.handleClick.bind(this);
         this.ele.addEventListener("click", this.handleClick)
     }
 
     handleClick() {
-        this.ele.children[0].innerText = "Eat Me"
+        this.ele.children[0].innerText = "🍪 Eat Me 🍪"
     }
 }
 
