bc2a3286486d9534857fa082209d94306ad49060
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-03-15 21:02:10 -0400

    Pre-changes

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let selectedWord = '';
   let selectedAdverb = '';
   let selectedLongWord = '';
-  // let selectedFrequent = '';
+  let selectedFrequent = '';
 
 
 
@@ -40,11 +40,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const numPairsElement = document.getElementById("num-pairs");
   const totalWordsElement = document.getElementById("total-words");
   const tenElement = document.getElementById("ten-words");
-  // const sixElement = document.getElementById("six-words");
   const adverbElement = document.getElementById("ly-words");
 
   adverbElement.textContent = combinedData.lyWords;
-  // sixElement.textContent = combinedData.sixWords;
   tenElement.textContent = combinedData.tenWords;
   totalWordsElement.textContent = combinedData.totalWords;
   numPairsElement.textContent = combinedData.numPairs;
@@ -103,7 +101,6 @@ const adverbsListItems = document.querySelectorAll("#adverbs li");
       selectedAdverb = adverb;
       selectedAdverb.classList.add("selected-adverb");
       const adverbText = selectedAdverb.textContent;
-      // console.log(`Selected adverb: ${adverbText}`);
 
       fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${adverbText}`)
       .then(response => response.json())
@@ -117,9 +114,6 @@ const adverbsListItems = document.querySelectorAll("#adverbs li");
     });
   });
 
-  
-
-  ////////////
   const selectedWords = document.querySelectorAll(".word");
   selectedWords.forEach((word) => {
     word.addEventListener("click", () => {
@@ -129,7 +123,6 @@ const adverbsListItems = document.querySelectorAll("#adverbs li");
       selectedWord = word;
       selectedWord.classList.add("selected-word");
       const wordText = selectedWord.textContent;
-      // console.log(`Selected word: ${wordText}`);
       
       fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${wordText}`)
         .then(response => response.json())
@@ -145,34 +138,32 @@ const adverbsListItems = document.querySelectorAll("#adverbs li");
 });
 
  
-  // const topWordsListItems = document.querySelectorAll("#frequent-words li");
+  const topWordsListItems = document.querySelectorAll("#frequent-words li");
 
-  // topWordsListItems.forEach((topWords) => {
-  //   const API_KEY = "4d51c794-eeb0-40fb-bef2-8b0605824280";
+  topWordsListItems.forEach((topWords) => {
+    const API_KEY = "4d51c794-eeb0-40fb-bef2-8b0605824280";
   
-  //   topWords.addEventListener("click", async () => {
-  //     if (selectedFrequent) {
-  //       selectedFrequent.classList.remove("selected-topword");
-  //     }
-  //     selectedFrequent = topWords;
-  //     selectedFrequent.classList.add("selected-topword");
-  //     const topWordText = selectedFrequent.textContent;
+    topWords.addEventListener("click", async () => {
+      if (selectedFrequent) {
+        selectedFrequent.classList.remove("selected-topword");
+      }
+      selectedFrequent = topWords;
+      selectedFrequent.classList.add("selected-topword");
+      const topWordText = selectedFrequent.textContent;
   
-  //     try {
-  //       const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${topWordText}?key=${API_KEY}`);
-  //       const data = await response.json();
+      try {
+        const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${topWordText}?key=${API_KEY}`);
+        const data = await response.json();
             
-  //       if (data.length > 0 && data[0].shortdef.length > 0) {
-  //         const definition = data[0].shortdef[0];
-  //         wordDefinitionBox.textContent = `${topWordText}: ${definition}`;
-  //       }
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   });
-  // });
+        if (data.length > 0 && data[0].shortdef.length > 0) {
+          const definition = data[0].shortdef[0];
+          wordDefinitionBox.textContent = `${topWordText}: ${definition}`;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  });
   
 
 
-
-  //////
